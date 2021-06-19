402d0d4dccb94e603e7b078f3afa0037a149593e
nks102000 <nitishks1110@gmail.com>
2021-06-19 23:00:18 +0530

    Components

--- app/src/main/java/in/iot/lab/acl/SubAdvanced/fragment_adv__sub__lottie.kt
+++ app/src/main/java/in/iot/lab/acl/SubAdvanced/fragment_adv__sub__lottie.kt
@@ -1,13 +1,14 @@
 package `in`.iot.lab.acl.SubAdvanced
 
 import `in`.iot.lab.acl.R
+import android.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.Fragment
-import io.noties.markwon.Markwon
+
+
 
 
 class fragment_adv__sub__lottie : Fragment() {
